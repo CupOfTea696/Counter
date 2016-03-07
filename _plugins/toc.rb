c28a522f102bc4a52af48d679dfa6d97b70163a9
CupOfTea696 <cupoftea696@gmail.com>
2016-03-07 22:24:35 +0000

    SubTOC for method listing

--- _plugins/toc.rb
+++ _plugins/toc.rb
@@ -8,10 +8,19 @@ module Jekyll
         doc = Nokogiri::HTML(html)
         doc.encoding = 'UTF-8'
        
-        toc_placeholder = doc.xpath("//comment()[contains(.,'TOC')]")
+        toc_placeholder = doc.xpath("//comment()[contains(.,'[[TOC]]')]")
+        sub_toc_placeholder = doc.xpath("//comment()[contains(.,'[[STOC]]')]")
        
-        if toc_placeholder
+        if toc_placeholder and sub_toc_placeholder
+            if toc_placeholder.count == 0 and sub_toc_placeholder.count == 0
+                return html
+            end
+        elsif toc_placeholder
             if toc_placeholder.count == 0
+                return html
+            end
+        elsif sub_toc_placeholder
+            if sub_toc_placeholder.count == 0
                 return html
             end
         else
@@ -62,17 +71,58 @@ module Jekyll
 
             item_number += 1;
         end
+       
+        sub_toc_html = ''
+        sub_item_number = 1
+       
+        doc.css('h4').each do |h4|
+            # TODO This XPATH expression can greatly improved
+            ct  = h4.xpath('count(following-sibling::h4)')
+            h5s = h4.xpath("following-sibling::h4[count(following-sibling::h2)=#{ct}]")
+
+            level_html = '';
+            inner_section = 0;
+
+            h5s.map.each do |h5|
+                inner_section += 1;
+
+                level_html += create_level_html(h5['id'],
+                    toc_level + 1,
+                    sub_item_number.to_s + '.' + inner_section.to_s,
+                    h5.text,
+                    '')
+            end
+            if level_html.length > 0
+                level_html = '<ul class="toc/level toc">' + level_html + '</ul>';
+            end
+
+            sub_toc_html += create_level_html(h4['id'],
+                toc_level,
+                sub_item_number,
+                h4.text,
+                level_html);
+
+            sub_item_number += 1;
+        end
 
         # for convenience item_number starts from 1
         # so we decrement it to obtain the index count
         toc_index_count = item_number - 1
+        sub_toc_index_count = sub_item_number - 1
        
-        if toc_html.length > 0
+        if toc_html.length > 0 or sub_toc_html.length > 0
             if min_items_to_show_toc <= toc_index_count
                 toc_table = TOC_CONTAINER_HTML
                     .gsub('%1', toc_html);
                 toc_placeholder.each do |placeholder|
                     placeholder.replace toc_table
+                end
+            end
+            if min_items_to_show_toc <= sub_toc_index_count
+                sub_toc_table = TOC_CONTAINER_HTML
+                    .gsub('%1', sub_toc_html);
+                sub_toc_placeholder.each do |placeholder|
+                    placeholder.replace sub_toc_table
                 end
             end
             doc.css('body').children.to_html
