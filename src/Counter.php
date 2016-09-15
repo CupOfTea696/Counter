3b433ce72530d07f1d203d0521f8fe389b2ebc36
Sven Wittevrongel <cupoftea696@gmail.com>
2016-09-15 13:12:00 +0100

    Release: v1.1.0

--- src/Counter.php
+++ src/Counter.php
@@ -26,7 +26,7 @@ class Counter implements SeekableIterator
      *
      * @const string
      */
-    const VERSION = '1.0.3';
+    const VERSION = '1.1.0';
     
     /**
      * The current position.
